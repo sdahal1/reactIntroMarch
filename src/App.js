8fa8e6e1530fcb9233385aaba4b4ebce783482d4
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.fios-router.home>
2021-03-10 12:55:51 -0500

    first commit for react intro

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
+
+  //pretend bootleg info from database User.objects.all()
+  let topBreakfastIdeas = [
+    {name: "French Toast", price: "5.99"},
+    {name: "Avocado Toast", price: "100"},
+    {name: "Pancakes", price: "8.99"},
+    {name: "Crepes", price: "25.99"},
+    {name: "Breakfast tacos", price: "3.50"}
+  ]
+  const x = "hi"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <ol>
+      
+      
+
+      {
+        topBreakfastIdeas.map(meal => {
+        return <li>{meal.name}</li>
+        })
+
+
+      }
+
+
+      
+    </ol>
     </div>
+    
   );
 }
 
